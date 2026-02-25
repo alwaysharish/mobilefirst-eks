c38bc80189d7ec54342448c3e6f8f8c143f1aebd
Harish Maddila <alwaysharish071@gmail.com>
2026-02-25 22:40:51 +0530

    Delete modules.tf

--- modules.tf
+++
@@ -1,8 +0,0 @@
-module "argocd" {
-  source = "./modules/argocd"
-    depends_on = [
-        helm_release.alb_controller,
-        #CRITICAL — wait for worker nodes
-        aws_eks_node_group.microservices
-     ]
-}
